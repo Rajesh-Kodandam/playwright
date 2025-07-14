e2d8255e2c823688dd7cff9795c80aee1881d540
Rajesh Kodandam <rajesh.kodandam@swedbank.se>
2025-07-14 14:22:23 +0200

    updated

---
+++ parameterized-fixtures/tests/base.ts
@@ -0,0 +1,22 @@
+import { test as base } from "@playwright/test"
+import { DashboardPage, User } from "./poms/dashboard"
+
+export type TestOptions = {
+  user: User
+  dashboardPage: DashboardPage
+}
+
+export const test = base.extend<TestOptions>({
+  user: [        
+    { email: "stefan@checklyhq.com", password: "..." },
+    { option: true },
+  ],
+
+  dashboardPage: async ({ page, user }, use) => {
+    const dashboardPage = new DashboardPage(page, user)
+    await dashboardPage.login()
+    await use(dashboardPage)
+  },
+})
+
+export { expect } from "@playwright/test"
